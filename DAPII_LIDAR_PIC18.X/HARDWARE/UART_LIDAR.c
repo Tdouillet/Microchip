c040f70d81d5a5b73a1f1bcd605f2b9e0b23caf6
Tdouillet <thibault.douillet1@gmail.com>
2024-10-23 14:39:15 +0200

    First commit
    First commit

---
+++ DAPII_LIDAR_PIC18.X/HARDWARE/UART_LIDAR.c
@@ -0,0 +1,11 @@
+/*
+ * File:   UART_LIDAR.c
+ * Author: thiba
+ *
+ * Created on 23 octobre 2024, 14:31
+ */
+
+
+#include <xc.h>
+
+
---
+++ DAPII_LIDAR_PIC18.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=DAPII_LIDAR_PIC18.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/DAPII_LIDAR_PIC18.X.production.hex
